a10a22f74d1e41dffccf66b768b9b170e8b60699
Pradip Khadka <fnfdrifter@gmail.com>
2018-04-10 18:07:25 +0545

    Initial

--- src/themes/default/app-extend.js
+++ src/themes/default/app-extend.js
@@ -1,7 +1,9 @@
 // You can extend main app.js file here. The function param is a root Vue instance.
 
 import Vuetify from 'vuetify'
+import Flickity from 'vue-flickity'
 
 export default function (Vue) {
   Vue.use(Vuetify)
+  Vue.component(Flickity)
 }
--- src/themes/default/index.js
+++ src/themes/default/index.js
@@ -1,8 +1,10 @@
 import UIStore from './store/ui-store'
 import VuetifyDemo from './pages/VuetifyDemo.vue'
+import FlickityDemo from './pages/FlickityDemo.vue'
 
 const routes = [
-  { path: '/', component: VuetifyDemo }
+  { path: '/', component: VuetifyDemo },
+  { path: '/flickity-demo', component: FlickityDemo }
 ]
 
 export default function (app, router, store) {
